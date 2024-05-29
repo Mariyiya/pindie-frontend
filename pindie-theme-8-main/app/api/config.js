97cf637aec58971e2f1e585233815793fb7a2cb5
Mariyiya <151542716+Mariyiya@users.noreply.github.com>
2024-05-29 23:41:37 +0700

    add

--- pindie-theme-8-main/app/api/config.js
+++ pindie-theme-8-main/app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:3001/api'
+export const BASE_URL = 'https://pindie-front-end.nomorepartiesco.ru'
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
